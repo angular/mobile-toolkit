7e10722c54e2e7fe0a39549728486626ae2b37fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-09-14 15:39:02 -0700

    refactor(worker): clean up old rxjs custom operators

--- service-worker/worker/src/worker/facade/fetch.ts
+++ service-worker/worker/src/worker/facade/fetch.ts
@@ -1,30 +1,26 @@
 import {Observable} from 'rxjs/Observable';
 import {NgSwAdapter} from './adapter';
-import {timeoutTo} from '../rxjs';
 
 export class NgSwFetch {
 
   constructor(private scope: ServiceWorkerGlobalScope, private adapter: NgSwAdapter) {}
 
-  request(req: Request, timeout: number = null): Observable<Response> {
+  request(req: Request): Observable<Response> {
     let result: Observable<Response> = Observable.defer(() => Observable
       .fromPromise<Response>(this
         .scope
         .fetch(req)));
-    if (timeout !== null) {
-      result = result.let<Response>(timeoutTo(timeout, undefined));
-    }
     return result;
   }
 
-  refresh(req: string | Request, timeout: number = null): Observable<Response> {
+  refresh(req: string | Request): Observable<Response> {
     let request: Request;
     if (typeof req == 'string') {
       request = this.adapter.newRequest(this._cacheBust(<string>req));
     } else {
       request = this.adapter.newRequest(this._cacheBust((<Request>req).url), <Request>req);
     }
-    return this.request(request, timeout);
+    return this.request(request);
   }
 
   private _cacheBust(url: string): string {
--- service-worker/worker/src/worker/rxjs/async.ts
+++
@@ -1,7 +0,0 @@
-import {Observable} from 'rxjs/Observable';
-
-export function doAsync<T>(fn: (T) => Observable<any>): any {
-  return (obs: Observable<T>) => obs
-    .concatMap(value => fn(value)
-      .reduce(() => value, value));
-}
