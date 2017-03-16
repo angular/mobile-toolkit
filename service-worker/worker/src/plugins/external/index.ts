a8dbdad8f2a8286618c3140f779a720b8588df08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-15 17:02:39 -0700

    feat(worker): an 'external' plugin which supports caching resources that have no build-time hash

---
+++ service-worker/worker/src/plugins/external/index.ts
@@ -0,0 +1,53 @@
+import {
+  FetchInstruction,
+  Operation,
+  Plugin,
+  PluginFactory,
+  VersionWorker,
+  VersionWorkerImpl,
+  cacheFromNetworkOp,
+  fetchFromCacheInstruction,
+} from '@angular/service-worker/worker';
+
+interface UrlConfig {
+  url: string;
+}
+
+interface ExternalManifest {
+  urls: UrlConfig[];
+}
+
+export interface ExternalContentCacheOptions {
+  manifestKey?: string;
+}
+
+export function ExternalContentCache(options?: ExternalContentCacheOptions): PluginFactory<ExternalPlugin> {
+  const manifestKey = (options && options.manifestKey) || 'external';
+  return (worker: VersionWorker) => new ExternalPlugin(worker as VersionWorkerImpl, manifestKey);
+}
+
+export class ExternalPlugin implements Plugin<ExternalPlugin> {
+  private cacheKey: string;
+
+  constructor(public worker: VersionWorkerImpl, public key: string) {
+    this.cacheKey = key === 'external' ? key : `external:${key}`;
+  }
+
+  private get externalManifest(): ExternalManifest {
+    return this.worker.manifest[this.key];
+  }
+
+  setup(operations: Operation[]) {
+    if (!this.externalManifest || !this.externalManifest.urls) {
+      return;
+    }
+    operations.push(...this
+      .externalManifest
+      .urls
+      .map(url => cacheFromNetworkOp(this.worker, url.url, this.cacheKey)));
+  }
+
+  fetch(req: Request, instructions: FetchInstruction[]): void {
+    instructions.unshift(fetchFromCacheInstruction(this.worker, req, this.cacheKey));
+  }
+}
